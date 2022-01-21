16c7973ee2c8c4428ee2e357887bfacc786ac7fe
Steven Maina <maichstivo254@gmail.com>
2022-01-21 16:00:20 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/love/it/love_it_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.love.it.love_it_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/domain/validators/validate_email.dart
@@ -0,0 +1,12 @@
+import 'package:dartz/dartz.dart';
+import 'package:love_it_app/domain/failures/value_failures.dart';
+
+Either<ValueFailure<String>, String> validateEmailAddress(String input) {
+  const String emailRegex =
+      r"""^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+""";
+  if (RegExp(emailRegex).hasMatch(input)) {
+    return right(input);
+  } else {
+    return left(ValueFailure<String>.invalidEmail(failedValue: input));
+  }
+}
